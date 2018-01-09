6c17da470dbd85ced89c227a0a26b6e70b789c05
Geoffrey Thomas <geoffrey.thomas@metaswitch.com>
2018-01-09 15:44:13 +0000

    Initial Files with working click prototype

---
+++ crystal_maze.js
@@ -0,0 +1,54 @@
+
+
+// how many people to put in the table?
+var table_length = 28;
+
+var tabledata;
+
+// An array of all results
+var all_results = [];
+
+// How many games to consider whether a player is active or not
+var activity_requirement = 500;
+
+// A list of all active players
+var active_players = [];
+
+//
+var activity_list = [];
+var inactivy_list = [];
+
+var s_db;
+var d_db;
+
+//
+// function: startup
+//
+// params: none
+// returns: nothing
+//
+// entry function called on startup
+//
+function startup()
+{
+  // load the json data
+}
+
+function addClickHandlers()
+{
+  //$('#add_single_left').on("click", function() {
+  //  $('#circle_1_1').css({ fill: "#ff0000" });
+  //});
+  $("#add_single_left").click(function() {
+    $("#circle_1_1").attr("fill", "red");
+  });
+
+  $("#add_single_right").click(function() {
+
+  });
+
+  $("#clear_results_players").click(function() {
+    clear_results_inputs();
+  });
+}
+
---
+++ readme.txt
@@ -0,0 +1,8 @@
+A Table Tennis ladder
+
+Start the server by running "./start" or "python resting.py". resting.py may
+need to be changed to choose a valid port and server host.
+
+Dependencies:
+- cherrypy
+- pickledb
