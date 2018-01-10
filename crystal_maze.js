48acfc49d851ca97989522b0a44dd7f6f7adef71
Geoffrey Thomas <geoffrey.thomas@metaswitch.com>
2018-01-10 18:30:22 +0000

    Grid generation working

--- crystal_maze.js
+++ crystal_maze.js
@@ -9,26 +9,25 @@ var columns = 20;
 
 function generate_grid()
 {
+
   var grid_svg = document.getElementById("lightboard_grid");
-  for (row = 1; row < rows + 1; row++)
+  for (row = 0; row < rows; row++)
   {
-    for (column = 1; column < columns + 1; column++)
+    for (column = 0; column < columns; column++)
     {
       var circle = document.createElementNS(svgNS, "circle");
       var id = "circle_" + row + "_" + column;
-      var centre_x = (column * 20);
-      var centre_y = (row * 20);
+      var centre_x = (column * 40) + 20;
+      var centre_y = (row * 40) + 20;
       var radius = 19;
       var colour = "red";
       var stroke = "black";
       var stroke_width = "2";
-      circle.setAttributeNS(svgNS, 'id', id);
-      circle.setAttributeNS(svgNS, 'cx', centre_x);
-      circle.setAttributeNS(svgNS, 'cy', centre_y);
-      circle.setAttributeNS(svgNS, 'r', radius);
-      circle.setAttributeNS(svgNS, 'fill', colour);
-      circle.setAttributeNS(svgNS, 'stroke', stroke);
-      circle.setAttributeNS(svgNS, 'stroke-width', stroke_width);
+      circle.setAttributeNS(null, 'id', id);
+      circle.setAttributeNS(null, 'cx', centre_x);
+      circle.setAttributeNS(null, 'cy', centre_y);
+      circle.setAttributeNS(null, 'r', radius);
+      circle.setAttributeNS(null, 'style', 'fill : red; stroke : black; stroke-width : 2;');
 
       grid_svg.appendChild(circle);
     }
