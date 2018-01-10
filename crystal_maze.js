8db78c8d09b357573d379b0a3de72069589a9e1b
Geoffrey Thomas <geoffrey.thomas@metaswitch.com>
2018-01-10 18:14:57 +0000

    Generate Grid

--- crystal_maze.js
+++ crystal_maze.js
@@ -1,47 +1,37 @@
-
-
-// how many people to put in the table?
-var table_length = 28;
-
-var tabledata;
-
-// An array of all results
-var all_results = [];
-
-// How many games to consider whether a player is active or not
-var activity_requirement = 500;
-
-// A list of all active players
-var active_players = [];
-
-//
-var activity_list = [];
-var inactivy_list = [];
-
-var s_db;
-var d_db;
-
-function addClickHandlers()
-{
-  //$('#add_single_left').on("click", function() {
-  //  $('#circle_1_1').css({ fill: "#ff0000" });
-  //});
-  $("#add_single_left").click(function() {
-    $("#circle_1_1").attr("fill", "red");
-  });
-
-
-  $("#add_single_right").click(function() {
-
-  });
-
-  $("#clear_results_players").click(function() {
-    clear_results_inputs();
-  });
-}
-
 function test()
 {
-    $("#circle_1_1").attr("fill", "red");
+    $("#circle_1_1").attr("fill", "black");
+}
+
+var namespace = "http://www.w3.org/2000/svg";
+var rows = 5;
+var columns = 20;
+
+function generate_grid()
+{
+  var grid_svg = document.getElementById("lightboard_grid");
+  for (row = 1; row < rows + 1; row++)
+  {
+    for (column = 1; column < columns + 1; column++)
+    {
+      var circle = document.createElementNS(svgNS, "circle");
+      var id = "circle_" + row + "_" + column;
+      var centre_x = (column * 20);
+      var centre_y = (row * 20);
+      var radius = 19;
+      var colour = "red";
+      var stroke = "black";
+      var stroke_width = "2";
+      circle.setAttributeNS(namespace, 'id', id);
+      circle.setAttributeNS(namespace, 'cx', centre_x);
+      circle.setAttributeNS(namespace, 'cy', centre_y);
+      circle.setAttributeNS(namespace, 'r', radius);
+      circle.setAttributeNS(namespace, 'fill', colour);
+      circle.setAttributeNS(namespace, 'stroke', stroke);
+      circle.setAttributeNS(namespace, 'stroke-width', stroke_width);
+
+      grid_svg.appendChild(circle);
+    }
+  }
 }
 
