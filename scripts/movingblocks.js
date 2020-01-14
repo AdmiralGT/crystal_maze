a7581b33b3af82729d51cce642be74de0d64dfaa
Geoff Thomas <admiralgt@gmail.com>
2020-01-14 22:17:01 +0000

    8 blocks, with randomized keys for testing

--- scripts/movingblocks.js
+++ scripts/movingblocks.js
@@ -8,14 +8,23 @@ var svg_height = 675
 
 class Block 
 {
-	constructor(id, x, y, speed, target, key) {
+	constructor(id, x, y, length, speed, target, key) {
 		this.block_id = id
 		this.x = x
 		this.y = y
+		this.length = length
 		this.speed = speed
-		this.active = true
+		this.target_id = target
 		this.key = key
-		this.placeBlock()
+		this.active = true
+		this.updateRight()
+		this.sizeBlock()
+	}
+
+	sizeBlock()
+	{
+
+		sizeObject(this.block_id, this.length)
 	}
 
 	stopBlock(keyID)
@@ -26,11 +35,17 @@ class Block
 		}
 	}
 
+	updateRight()
+	{
+		this.right = this.x + this.length
+	}
+
 	moveBlock()
 	{
 		if (this.active)
 		{
 			this.x = this.x + this.speed
+			this.updateRight()
 			this.placeBlock()
 			this.flipBlock()
 		}
@@ -48,15 +63,27 @@ class Block
 	{
 		placeObject(this.block_id, this.x)
 	}
+
+	inTarget()
+	{
+		if ((this.x > this.target.left) && (this.x < this.target.right))
+		{
+			return true
+		}
+		return false
+	}
 }
 
 class Target
 {
-	constructor(id, x, y)
+	constructor(id, x, y, length)
 	{
 		this.target_id = id
 		this.x = x
 		this.y = y
+		this.length = length
+		this.left = this.x + 2
+		this.right = this.x + length - 4
 		this.placeTarget()
 	}
 
@@ -71,6 +98,12 @@ function placeObject(id, x)
 {
 	var object = document.getElementById(id)
 	object.setAttribute("x", x.toString())
+}
+
+function sizeObject(id, width)
+{
+	var object = document.getElementById(id)
+	object.setAttribute("width", width.toString())
 }
 
 function generate_game()
@@ -96,7 +129,15 @@ function initializeGame()
 
 function initializeBlocks()
 {
-	blocks.push(new Block("blue", 5, 2, 2, null, 90))
+	// x, y, length, speed, target, key
+	blocks.push(new Block("blue", 5, 2, 80, 2, null, 58))
+	blocks.push(new Block("red", 5, 2, 80, 2, null, 49))
+	blocks.push(new Block("yellow", 5, 2, 80, 2, null, 52))
+	blocks.push(new Block("limegreen", 5, 2, 80, 2, null, 74))
+	blocks.push(new Block("cyan", 5, 2, 80, 2, null, 84))
+	blocks.push(new Block("darkorange", 5, 2, 80, 2, null, 60))
+	blocks.push(new Block("purple", 5, 2, 80, 2, null, 87))
+	blocks.push(new Block("hotpink", 5, 2, 80, 2, null, 80))
 }
 
 function initializeTargets()
@@ -104,9 +145,30 @@ function initializeTargets()
 	return
 }
 
+function check_result()
+{
+	for (var ii = 0; ii < blocks.length; ii++)
+	{
+		block = blocks[ii]
+		// If any block is active, we can't be winning
+		if (block.active)
+		{
+			return
+		}
+
+		// If the block isn't in the target, then we can't win
+		if (!block.inTarget())
+		{
+			return
+		}
+	}
+	alert("The crystal is yours!")
+}
+
 function stopBlocks(keyID)
 {
 	blocks.forEach(block => block.stopBlock(keyID))
+	check_result()
 }
 
 function resetBlocks()
